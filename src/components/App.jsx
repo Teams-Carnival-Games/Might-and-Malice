231b3d87dcfba571ed9a078ad72652827f6c1422
uzi-a <84665769+uzi-a@users.noreply.github.com>
2023-08-28 19:42:10 +0100

    Moved to Teams Carnival group

---
+++ src/components/App.jsx
@@ -0,0 +1,71 @@
+// https://fluentsite.z22.web.core.windows.net/quick-start
+import {
+  FluentProvider,
+  teamsLightTheme,
+  teamsDarkTheme,
+  teamsHighContrastTheme,
+  tokens,
+} from "@fluentui/react-components";
+import { useEffect } from "react";
+import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import { app } from "@microsoft/teams-js";
+import { useTeamsUserCredential } from "@microsoft/teamsfx-react";
+import Privacy from "./Privacy";
+import TermsOfUse from "./TermsOfUse";
+import TurnBasedCombat from "./TurnBasedCombat";
+import TabConfig from "./TabConfig";
+import { TeamsFxContext } from "./Context";
+import config from "./sample/lib/config";
+
+
+/**
+ * The main app which handles the initialization and routing
+ * of the app.
+ */
+export default function App() {
+  const { loading, theme, themeString, teamsUserCredential } = useTeamsUserCredential({
+    initiateLoginEndpoint: config.initiateLoginEndpoint,
+    clientId: config.clientId,
+  });
+  useEffect(() => {
+    loading &&
+      app.initialize().then(async () => {
+        // Hide the loading indicator.
+        app.notifySuccess();
+      });
+  }, [loading]);
+
+  window.addEventListener('error', function (event) {
+    console.error("Caught a global error");
+  });
+  
+  return (
+    <TeamsFxContext.Provider value={{ theme, themeString, teamsUserCredential }}>
+      <FluentProvider
+        theme={
+          themeString === "dark"
+            ? teamsDarkTheme
+            : themeString === "contrast"
+            ? teamsHighContrastTheme
+            : {
+                ...teamsLightTheme,
+                colorNeutralBackground3: "#eeeeee",
+              }
+        }
+        style={{ background: tokens.colorNeutralBackground3 }}
+      >
+        <Router>
+          {!loading && (
+            <Routes>
+              <Route path="/privacy" element={<Privacy />} />
+              <Route path="/termsofuse" element={<TermsOfUse />} />
+              <Route path="/tab" element={<TurnBasedCombat />} />
+              <Route path="/config" element={<TabConfig />} />
+              <Route path="*" element={<Navigate to={"/tab"} />}></Route>
+            </Routes>
+          )}
+        </Router>
+      </FluentProvider>
+    </TeamsFxContext.Provider>
+  );
+}
